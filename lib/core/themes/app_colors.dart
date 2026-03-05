f80140fc3e5aea199d9b7a89befbfc3bae37efc0
VCodes0 <orewavishnuprajapati@gmail.com>
2026-03-05 16:41:20 +0530

    Add APPColors

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color.fromRGBO(25, 26, 26, 1);
+  static const Color sideNav = Color.fromRGBO(32, 34, 34, 1);
+  static const Color searchBar = Color.fromRGBO(32, 34, 34, 1);
+  static const Color searchBarBorder = Color.fromRGBO(60, 63, 64, 1);
+  static const Color iconGrey = Color(0xFF909090);
+  static const Color textGrey = Color(0xFFAAAAAA);
+  static const Color footerGrey = Color(0xFF737373);
+  static const Color proButton = Color.fromRGBO(47, 48, 47, 1);
+  static const Color cardColor = Color(0xFF262626);
+  static const Color submitButton = Color.fromRGBO(27, 185, 206, 1);
+  static const Color whiteColor = Color.fromRGBO(255, 255, 255, 1.0);
+}
