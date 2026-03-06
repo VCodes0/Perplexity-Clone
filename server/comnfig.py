27572032c9c1a37ffd3e1b4de66f9b9a2ba42aef
VCodes0 <orewavishnuprajapati@gmail.com>
2026-03-06 12:56:33 +0530

    Just Ui

--- server/comnfig.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
-
-load_dotenv()
-
-class Settings(BaseSettings):
-    GEMINI_API_KEY : str = ""
--- server/services/search_services.py
+++
@@ -1,7 +0,0 @@
-from comnfig import Settings
-
-
-setting = Settings()
-class SearchService:
-    def web_search(self, query: str):
-        print(setting.GEMINI_API_KEY)
